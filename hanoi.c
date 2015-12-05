fc62247eae1586dfdd5388f6d00c9e92955db468
Celthi <txtlu0@gmail.com>
2015-12-05 20:24:06 +0800

    after watching SICP, change hanoi to a better and simple form

--- hanoi.c
+++ hanoi.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*************************************
+ * first version
+ * second version using three spare arguments
+ ************************************/
 void hanoi(int n, char src, char dest);
+void hanoi_2(int n, char src, char dest, char spare);
 void mov(char src, char dest);
 int main(int argc, const char *argv[])
 {
-    hanoi(5, 'B', 'C');
+    hanoi_2(3, 'B', 'C', 'A');
+	printf("==============\n");
+	hanoi(3, 'B', 'C');
     return 0;
 }
 
 
+void hanoi_2(int n, char src, char dest, char spare)
+{
+	if (n == 0) {
+		return;
+	}
+	else {
+		hanoi_2(n-1, src, spare, dest);
+		mov(src, dest);
+		hanoi_2(n-1, spare, dest, src);
+	}
+}
 void hanoi(int n, char src, char dest)
 {
     char temp;
@@ -37,3 +55,4 @@ void mov(char src, char dest)
     printf("move %c to %c\n", src, dest);
 	return;
 }
+/****************************/
